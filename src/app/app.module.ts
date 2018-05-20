00018ed6c58931783de9ca76a807a78b72e73e6e
sandeepa amcp <sandeepaamcp@gmail.com>
2018-05-20 11:34:24 +0530

    map loading errors fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClient } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,7 +17,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Rx';
+
+declare const google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  locations = ["Karapitiya", "Horana", "Homagama", "Rajagiriya", "Kandy", "Nuwara eliya"];
 
-  constructor(public navCtrl: NavController) {
+  mapsAPIKey = 'YOUR-API-KEY-HERE';
+  coordinates: any = [];
+  resTmp: any;
 
+  //for google map view
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.loadMap();
   }
+  loadMap() {
+    Observable.forkJoin(
+      this.locations.map(
+        i => this.http.get("https://maps.googleapis.com/maps/api/geocode/json?address=" +
+          i + "&key=" + this.mapsAPIKey)
+          .map(res => {
+            this.resTmp = res;
+            if (this.resTmp.results.length != 0) {
+              this.coordinates.push(this.resTmp.results[0].geometry.location);
+            }
+          })
+      )
+    ).subscribe(res => {
+      let galle = { lat: 6.0765847, lng: 80.1958755 }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 8,
+        center: galle,
+        mapTypeId: 'roadmap'
+      });
+      for (var i = 0; i < this.coordinates.length; i++) {
+        var marker = new google.maps.Marker({
+          position: this.coordinates[i],
+          map: this.map
+        });
+      }
+      this.map.setCenter(galle);
 
+    });
+  }
 }
